52b0b3a984e4637766939e64e8554d06106ba9a9
ritesh <ritesh.gulghane@trayak.com>
2022-04-07 19:45:37 +0530

    wheather

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hello Ritesh");
+// ----------- Wheather App ----------------
+
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+geocode('New York', (err, data) => {
+    console.log('data - ', data);
+    forecast(data.latitude, data.longitude, (error, data) => {
+        if (error) {
+            console.log('Error', error);
+        } else {
+            console.log('Data', data)
+        }
+    })
+})
+
+const add = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b);
+    }, 2000);
+}
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
+
